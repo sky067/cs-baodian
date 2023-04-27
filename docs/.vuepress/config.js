b3c25596b7a9ee893b917b2bc2ce8575d171dc5a
w <xingao1406@163.com>
2023-04-27 20:01:29 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,6 @@ import { defineUserConfig } from 'vuepress'
 
 export default defineUserConfig({
   lang: 'zh-CN',
-  title: 'VuePress',
+  title: 'VuePress CS基础教程',
   description: '这是我的第一个 VuePress 站点',
 })
